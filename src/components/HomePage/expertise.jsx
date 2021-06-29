ef7d53d0d3e5c73ecf8377cd1e0c4ec117dd31f2
Luis Fernando Saavedra Meza <dev@fsvdr.me>
2021-06-29 12:11:29 -0500

    Fix stats background

--- src/components/HomePage/expertise.jsx
+++ src/components/HomePage/expertise.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Pattern from '../Pattern';
-import ShapeMask from '../../images/shape.svg';
 
 const Expertise = () => {
   return (
@@ -22,25 +21,23 @@ const Expertise = () => {
         easy navigation, student interaction and lesson activities.
       </p>
 
-      <ShapeMask className />
-
       <div className="flex flex-col items-stretch pt-32 md:flex-row md:justify-evenly md:items-start lg:items-center lg:py-48">
         <p className="flex flex-row items-start md:flex-col md:text-center md:items-center md:flex-1 lg:flex-row mb-16 md:mb-0 md:mr-16 lg:text-left">
-          <span className="clip-shape flex justify-start pl-7 pt-2 items-center min-w-32 h-20 font-semibold text-xl text-white bg-primary mr-7 md:mr-0 md:mb-7 lg:mb-0 lg:mr-7">
+          <span className="bg-shape flex justify-start pl-7 pt-2 items-center min-w-32 h-20 font-semibold text-xl text-white mr-7 bg-no-repeat bg-center md:mr-0 md:mb-7 lg:mb-0 lg:mr-7">
             +10
           </span>
           <span className="text-md">Years of alternative provision both online and offline</span>
         </p>
 
         <p className="flex flex-row items-start md:flex-col md:text-center md:items-center md:flex-1 lg:flex-row mb-16 md:mb-0 md:mr-16 lg:text-left">
-          <span className="clip-shape flex justify-start pl-7 pt-2 items-center min-w-32 h-20 font-semibold text-xl text-white bg-primary mr-7 md:mr-0 md:mb-7 lg:mb-0 lg:mr-7">
+          <span className="bg-shape flex justify-start pl-7 pt-2 items-center min-w-32 h-20 font-semibold text-xl text-white mr-7 bg-no-repeat bg-center md:mr-0 md:mb-7 lg:mb-0 lg:mr-7">
             700
           </span>
           <span className="text-md">Pupils a year either educational support or alternative online provision</span>
         </p>
 
         <p className="flex flex-row items-start md:flex-col md:text-center md:items-center md:flex-1 lg:flex-row lg:text-left">
-          <span className="clip-shape flex justify-start pl-7 pt-2 items-center min-w-32 h-20 font-semibold text-xl text-white bg-primary mr-7 md:mr-0 md:mb-7 lg:mb-0 lg:mr-7">
+          <span className="bg-shape flex justify-start pl-7 pt-2 items-center min-w-32 h-20 font-semibold text-xl text-white mr-7 bg-no-repeat bg-center md:mr-0 md:mb-7 lg:mb-0 lg:mr-7">
             45
           </span>
           <span className="text-md">Local authorities supported</span>
