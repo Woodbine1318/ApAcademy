2b96a05904a62d8754da048e2bdaedcb972a6d51
Luis Fernando Saavedra Meza <dev@fsvdr.me>
2021-07-14 13:20:37 -0500

    Update footer layout

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -35,40 +35,40 @@ const Footer = () => {
 
   return (
     <footer className="bg-primary-200 pt-16 pb-4 px-constrained md:px-constrained-md md:pb-16 lg:px-constrained-lg">
-      <div className="md:flex flex-row flex-nowrap justify-between items-end md:mb-12">
+      <div className="md:flex flex-row flex-nowrap justify-between md:mb-12">
         <div>
           <Link to="/" aria-label="Home" className="block mb-12">
             <Logo />
           </Link>
 
-          <nav className="mb-28 md:mb-0">
-            <ul>
-              {footer.links.map((link) =>
-                link.__typename === 'ContentfulLink' ? (
-                  <li className="text-md" key={link.id}>
-                    <Link to={link.url ?? `/blog/${link.entry?.slug}`}>{link.text}</Link>
-                  </li>
-                ) : (
-                  <li className="text-md" key={link.id}>
-                    <Link to={`/${link.slug}`}>{link.title}</Link>
-                  </li>
-                ),
-              )}
-            </ul>
-          </nav>
+          <div className="flex flex-col mb-16 md:mb-0">
+            <a href={`tel:${phone}`} className="flex flex-nowrap items-center mb-7">
+              <PhoneIcon className="mr-6" />
+              <span className="underline">{phone}</span>
+            </a>
+
+            <a href={`mailto:${email}`} className="flex flex-nowrap items-center">
+              <MailIcon className="mr-6" />
+              <span className="underline">{email}</span>
+            </a>
+          </div>
         </div>
 
-        <div className="flex flex-col items-end mb-8 md:mb-0">
-          <a href={`tel:${phone}`} className="flex flex-nowrap items-center mb-7 md:mb-0">
-            <PhoneIcon className="mr-6" />
-            <span className="underline">{phone}</span>
-          </a>
-
-          <a href={`mailto:${email}`} className="flex flex-nowrap items-center">
-            <MailIcon className="mr-6" />
-            <span className="underline">{email}</span>
-          </a>
-        </div>
+        <nav className="mb-12 md:mb-0">
+          <ul>
+            {footer.links.map((link) =>
+              link.__typename === 'ContentfulLink' ? (
+                <li className="text-md" key={link.id}>
+                  <Link to={link.url ?? `/blog/${link.entry?.slug}`}>{link.text}</Link>
+                </li>
+              ) : (
+                <li className="text-md" key={link.id}>
+                  <Link to={`/${link.slug}`}>{link.title}</Link>
+                </li>
+              ),
+            )}
+          </ul>
+        </nav>
       </div>
 
       <small className="block text-right border-t border-primary-100 pt-8">
